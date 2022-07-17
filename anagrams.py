fea4d397e04a58a827a86475b6bda028f7b3629b
phaggi <phaggi@gmail.com>
2022-07-17 23:06:15 +0300

    refactor class Vocabularity for empty strings in word

--- anagrams.py
+++ anagrams.py
@@ -22,27 +22,36 @@ class Nouns:
 
 
 class Vocabulary:
-    word = None
     input_text = 'Введите русское слово существительное:'
     nouns = None
 
-    def __init__(self, full_nouns):
-        self.full_nouns = full_nouns
+    def __init__(self, full_nouns=None):
+        self.word = None
+        if full_nouns is None:
+            self.full_nouns = Nouns().full_nouns
+        else:
+            self.full_nouns = full_nouns
 
     def set_word(self):
-        while self.word not in self.full_nouns:
+        while self.word not in self.full_nouns and self.word != '':
             self.word = input(self.input_text).lower()
+
 
     def set_nouns(self):
         self.nouns = {item for item in self.full_nouns if len(item) == len(self.word)}
 
-    def find_anagrams(self) -> set:
-        self.set_word()
+    def find_anagrams(self, word: str = None) -> set:
+        if word is None and self.word != '':
+            self.set_word()
+        elif word.isalpha() and word.lower() in self.full_nouns:
+            self.word = word.lower()
+        if self.word == '':
+            raise Exception('Конец работы.')
         self.set_nouns()
         return {''.join(i) for i in permutations(list(self.word))} & self.nouns
 
 
 if __name__ == '__main__':
-    nouns = Nouns().full_nouns
-    voc = Vocabulary(nouns)
-    print('\n'.join(sorted(list(voc.find_anagrams()))))
+    find_anagrams('столик')
+    find_anagrams()
+    find_anagrams(cycle=True)
